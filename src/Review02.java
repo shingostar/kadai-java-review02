27503c1547fc4fee6ddc96b6cc8794a0edbb6954
shingostar <shingostar@shore.ocn.ne.jp>
2023-12-16 13:28:01 +0900

    Review02.javaの作成が完成しました。

--- src/Review02.java
+++ src/Review02.java
@@ -2,8 +2,20 @@
 public class Review02 {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
-
-    }
-
+     // iの値が0から100より小さい間、処理を繰り返す
+        for(int i = 1; i <= 100; i++) {
+            // iの値が3かつ5で割り切れるとき
+         if (i % 3 == 0 && i % 5 == 0) {
+            System.out.println("FizzBizz");
+            // iの値が3で割り切れるとき
+            }else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            // iの値が5で割り切れるとき
+            } else if (i % 5 == 0) {
+                System.out.println("Bizz");
+            }else {
+                System.out.println(i);
+            }
+       }
+   }
 }
